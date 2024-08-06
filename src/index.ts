5f531cd1a1131c15bed7f6fe3cfa185654420a76
Kamil Ratowski <kamilratowski90@gmail.com>
2024-08-06 11:09:11 +0200

    Added handling of -c flag for showing file size in bytes

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,35 @@
 #!/usr/bin/env node
+import fs from "node:fs/promises";
 
 // we get third argument here as first one would be 'node' and the second one the script file we run
 function main(): void {
   const args = process.argv.slice(2);
+  const wcArg = args[0];
+  const filePath = args[1];
 
-  const wcArgument = args[0];
-
-  if (wcArgument == "-c") {
-    countBytes();
+  if (wcArg == "-c") {
+    showFileBytes(filePath);
   }
 }
 
-function countBytes(): void {
-  console.log("we are logging bytes");
+async function showFileBytes(filePath: string): Promise<void> {
+  console.log("Reading file...");
+
+  const fileBytes = await countBytes(filePath);
+
+  if (fileBytes) {
+    console.log(`${fileBytes} ${filePath}`);
+  }
+}
+
+async function countBytes(filePath: string): Promise<number | null> {
+  try {
+    const stats = await fs.stat(filePath);
+    return stats.size;
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
 }
 
 main();
